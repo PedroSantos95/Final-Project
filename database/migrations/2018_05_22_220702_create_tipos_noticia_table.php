b10d2e0bf3b055b3fb5351ac032c0eee8dafcedb
miguel851212 <mike851212@hotmail.com>
2018-05-22 22:07:29 +0100

    agora fudeu muleke

---
+++ database/migrations/2018_05_22_220702_create_tipos_noticia_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateTiposNoticiaTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tipos_noticia', function(Blueprint $table)
+		{
+			$table->boolean('id_tipo_noticia')->primary();
+			$table->string('nome', 40);
+			$table->string('path_white', 100);
+			$table->string('path_black', 100);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tipos_noticia');
+	}
+
+}
---
+++ database/migrations/2018_05_22_220703_add_foreign_keys_to_carros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddForeignKeysToCarrosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('carros', function(Blueprint $table)
+		{
+			$table->foreign('id_rally', 'PK_carros_rallys')->references('id_rally')->on('rallys')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('carros', function(Blueprint $table)
+		{
+			$table->dropForeign('PK_carros_rallys');
+		});
+	}
+
+}
