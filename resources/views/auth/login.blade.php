dfc93561919506525f1128bc1361b5f0f1258bc8
PedroSantos95 <pedromverasantos@gmail.com>
2018-07-16 17:59:53 +0100

    remove tempos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,8 +13,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{route('tempos')}}">Tempos
-                    </a>
+
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('noticias')}}">Noticias</a>
