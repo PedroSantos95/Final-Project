5e5a3df73f7a2b0c6f6e13d9779b35c5c269a5fa
PedroSantos95 <pedromverasantos@gmail.com>
2018-08-29 20:30:20 +0100

    admin bugfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Autenticação Administração') }}</title>
+    <title>Autenticação Administração</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
