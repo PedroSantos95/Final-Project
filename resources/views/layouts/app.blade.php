2317ba7dad78db684a08a119344f06a253ac7997
PedroSantos95 <pedromverasantos@gmail.com>
2018-08-29 20:31:20 +0100

    123123

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Autenticação Administração</title>
+    <title>Administração</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
