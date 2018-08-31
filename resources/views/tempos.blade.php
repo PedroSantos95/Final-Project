b12bad608f2ad4555ac50e888c431328fbcb0fee
PedroSantos95 <pedromverasantos@gmail.com>
2018-08-31 18:56:05 +0100

    resizes

--- resources/views/tempos.blade.php
+++ resources/views/tempos.blade.php
@@ -136,8 +136,8 @@
            id="historicoNoticias" cellspacing="0" width="100%">
         <thead>
         <tr>
-            <th width="20%" style="text-align: center;">Data/Hora</th>
-            <th width="80%" style="text-align: center;">Notícia</th>
+            <th width="25%" style="text-align: center;">Data/Hora</th>
+            <th width="75%" style="text-align: center;">Notícia</th>
         </tr>
             @for ($i = 0; $i <sizeof($historicoNoticias); $i++)  
             <tr>
