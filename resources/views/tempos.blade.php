5f195b9df1c889d6c2d99b4904fabc838123830d
PedroSantos95 <pedromverasantos@gmail.com>
2018-09-09 00:00:05 +0100

    1

--- resources/views/tempos.blade.php
+++ resources/views/tempos.blade.php
@@ -109,7 +109,7 @@
                 <td v-for="index2 in numeroTemposIntermedios">
                     <!--<span style="color:red; font-weight: bold" v-if="index != carroRef && getNameField(value, index2).indexOf('+') !== -1">@{{ getNameField(value, index2) }}</span> <!-->    
                     <span style="content: 'center' ;font-weight:bold" v-if="index == carroRef">@{{ getCarRefTimes(value, index2)}}</span>
-                    <span :style="{'color:red; font-weight:bold': index != carroRef && getNameField(value, index2).indexOf('+') !== -1, 'color:green; font-weight:bold' : index != carroRef && getNameField(value, index2).indexOf('-') !== -1}" v-else>
+                    <span :style="{'color:red': index != carroRef && getNameField(value, index2).indexOf('+') !== -1, 'color:green' : index != carroRef && getNameField(value, index2).indexOf('-') !== -1}" v-else>
                     @{{ getNameField(value, index2) }}
                     </span>
                 </td>
