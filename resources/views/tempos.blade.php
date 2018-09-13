53e9ad299bf2f6ed3499f102d9fc28364078e177
PedroSantos95 <pedromverasantos@gmail.com>
2018-09-13 09:38:58 +0100

    213

--- resources/views/tempos.blade.php
+++ resources/views/tempos.blade.php
@@ -71,7 +71,6 @@
 
 <!-- Page Content -->
 
-       <h3 style="margin-top:10px;">Notícias</h3> 
 <div class="container" id="app">
     <div class="row">
         <div class="col-sm-4 col-sm-offset-4 col-lg-12">
